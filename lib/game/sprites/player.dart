16ca0b590130358d43b36a2e699f260f6ae5a700
Dave Faliskie <davefaliskie@gmail.com>
2024-02-16 00:53:03 +0100

    add some boundries to the Player

--- lib/game/sprites/player.dart
+++ lib/game/sprites/player.dart
@@ -6,18 +6,30 @@ class Player extends SpriteComponent with HasGameRef {
     sprite = await Sprite.load('bottle.png');
     size = Vector2.all(100);
     position = Vector2(gameRef.size.x / 2, 0 + size.y / 2);
-    anchor = Anchor.topCenter;
+    anchor = Anchor.center;
   }
 
   @override
   void update(double dt) {
     super.update(dt);
     // This makes the sprite fall down
-    position.add(Vector2(0, 100 * dt));
+    double newYPosition = position.y + (300 * dt);
+
+    // Check if the new Y position is greater than the game height minus half the sprite size
+    // If so, set the Y position to the bottom of the game area
+    if (newYPosition > gameRef.size.y - size.y / 2) {
+      newYPosition = gameRef.size.y - size.y / 2;
+    }
+
+    // Apply the calculated or adjusted Y position
+    position.y = newYPosition;
   }
 
   void move(double deltaX) {
-    // Move the player left or right based on the drag delta
-    position.add(Vector2(deltaX * 0.5, 0));
+    final newX = position.x + deltaX;
+
+    if (newX >= size.x / 2 && newX <= gameRef.size.x - size.x / 2) {
+      position.add(Vector2(deltaX, 0));
+    }
   }
 }
