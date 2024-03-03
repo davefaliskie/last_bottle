031b01513d771468368f739a9dfa723a0c4b8337
Dave Faliskie <davefaliskie@gmail.com>
2024-03-03 16:23:09 +0000

    Simplify bottle intro to be images & not FlameGame

--- lib/bottle_game/bottle.dart
+++ lib/bottle_game/bottle.dart
@@ -1,11 +1,12 @@
 import 'package:flame/components.dart';
 import 'package:last_bottle/bottle_game/bottle_game.dart';
+import 'package:last_bottle/constants.dart';
 
 class Bottle extends SpriteComponent with HasGameRef<BottleGame> {
   @override
   Future<void> onLoad() async {
     sprite = await Sprite.load('display/bottle_full.png');
-    size = Vector2.all(300);
+    size = Vector2.all(gameWidth * 0.85);
     position = Vector2(0, 0);
     anchor = Anchor.center;
   }
--- lib/bottle_game/bottle_game_screen.dart
+++ lib/bottle_game/bottle_game_screen.dart
@@ -1,11 +1,20 @@
 import 'package:flame/game.dart';
+import 'package:flame_audio/flame_audio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:last_bottle/bottle_game/bottle_game.dart';
 import 'package:last_bottle/constants.dart';
+import 'package:last_bottle/local_data/data/hive_repository.dart';
 import 'package:last_bottle/router.dart';
+
+enum BottleState {
+  closed,
+  opened,
+  spilling,
+  empty,
+}
 
 class BottleGameScreen extends ConsumerStatefulWidget {
   const BottleGameScreen({super.key});
@@ -15,12 +24,15 @@ class BottleGameScreen extends ConsumerStatefulWidget {
 }
 
 class _BottleGameScreenState extends ConsumerState<BottleGameScreen> {
-  late BottleGame game;
+  // late BottleGame game;
+  BottleState bottleState = BottleState.closed;
+  // Widget? displayBottleWidget;
 
   @override
   void initState() {
     super.initState();
-    game = getGame();
+    // game = getGame();
+    // displayBottleWidget = _bottleClosed();
 
     // todo what music do we want.
     // if (ref.read(hiveRepositoryProvider).playSound) {
@@ -30,30 +42,125 @@ class _BottleGameScreenState extends ConsumerState<BottleGameScreen> {
   }
 
   // Handle the game's End Logic in this callback to access context & ref
-  BottleGame getGame() {
-    return BottleGame(
-      endAndGo: () {
-        // todo save completion to hive & then use it to not show this screen again.
-        HapticFeedback.heavyImpact();
-        context.goNamed(AppRoute.endTrash.name);
-      },
-    );
-  }
+  // BottleGame getGame() {
+  //   return BottleGame(
+  //     endAndGo: () {
+  //       // todo save completion to hive & then use it to not show this screen again.
+  //       HapticFeedback.heavyImpact();
+  //       context.goNamed(AppRoute.endTrash.name);
+  //     },
+  //   );
+  // }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Center(
-          child: FittedBox(
-            child: SizedBox(
-              height: gameHeight,
-              width: gameWidth,
-              child: GameWidget(game: game),
-            ),
+      appBar: AppBar(
+        title: Text(bottleState.name),
+        centerTitle: true,
+      ),
+      bottomNavigationBar: SafeArea(
+        child: Container(
+          height: 75,
+          color: Colors.green,
+          child: Center(
+            child: Text(_hintText()),
           ),
         ),
       ),
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Spacer(),
+            Container(
+              color: Colors.white,
+              child: AnimatedSwitcher(
+                duration: const Duration(seconds: 2),
+                child: _displayBottleWidget(),
+              ),
+            ),
+            const Spacer(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _displayBottleWidget() {
+    switch (bottleState) {
+      case BottleState.opened:
+        return _bottleOpened();
+      default:
+        // BottleState.closed
+        return _bottleClosed();
+    }
+  }
+
+  String _hintText() {
+    switch (bottleState) {
+      case BottleState.opened:
+        return "Drink The Water";
+      default:
+        // BottleState.closed
+        return "Open The Bottle";
+    }
+  }
+
+  // Closed bottle action -> Open Bottle
+  Widget _bottleClosed() {
+    return Stack(
+      alignment: Alignment.topCenter,
+      children: [
+        Image.asset(
+          "assets/images/bottles/closed.png",
+        ),
+        Padding(
+          padding: const EdgeInsets.only(top: 70.0),
+          child: InkWell(
+            child: Container(
+              // color: Colors.green.withOpacity(0.3),
+              color: Colors.transparent,
+              height: 40,
+              width: 60,
+            ),
+            onTap: () async {
+              debugPrint("Opening Bottle");
+              if (true || ref.read(hiveRepositoryProvider).playSound) {
+                await FlameAudio.play('pop.mp3');
+              }
+              setState(() {
+                bottleState = BottleState.opened;
+              });
+            },
+          ),
+        )
+      ],
+    );
+  }
+
+  // Opened bottle action -> Spilling Bottle
+  Widget _bottleOpened() {
+    return Stack(
+      alignment: Alignment.topCenter,
+      children: [
+        Image.asset(
+          "assets/images/bottles/opened.png",
+        ),
+        // InkWell(
+        //   child: Container(
+        //     color: Colors.red.withOpacity(0.3),
+        //     height: 50,
+        //     width: 70,
+        //   ),
+        //   onTap: () {
+        //     debugPrint("Opening Bottle");
+        //     setState(() {
+        //       bottleState = BottleState.opened;
+        //     });
+        //   },
+        // )
+      ],
     );
   }
 }
